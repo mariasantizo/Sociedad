b6196297f5354dc4d0ccb055df83a6d97a4098b7
Maria <maria.santizo@opendeusto.es>
2021-10-22 12:31:44 +0200

    add Persona

---
+++ src/datos/Persona.java
@@ -0,0 +1,61 @@
+package datos;
+
+public class Persona {
+	private String dni;
+	private String nombre;
+	private String apellido;
+	private int telefono;
+	private String direccion;
+	
+	public Persona(String dni, String nombre, String apellido, int telefono, String direccion) {
+		super();
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.telefono = telefono;
+		this.direccion = direccion;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public int getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	
+	
+	
+}
