cd23334478684317c3ad211c74e3143005cd65fe
mariasantizo <maria.santizo@opendeusto.es>
2021-10-30 12:57:53 +0200

    Add calcularCuota

--- src/datos/ProgramaPrincipal.java
+++ src/datos/ProgramaPrincipal.java
@@ -9,11 +9,12 @@ public class ProgramaPrincipal {
 		// TODO Auto-generated method stub
 		
 		Administrador a1 = new Administrador("72456413H", "Maria", "Santizo", 673893521, "Mundaiz 3");
-		Socio s1 = new Socio("72451111R", "Ana", "Garcia", 673111111, "Mundaiz 3", 1);
+		Socio s1 = new Socio("72451111R", "Ana", "Garcia", 673111111, "Mundaiz 3", 1, "Semestral");
 		Mesa m1 = new Mesa(1, 10);
 		Calendar f1 = new GregorianCalendar (2021,10,30);
 		Reserva r1 = new Reserva (s1, m1, f1, "Comida");
-		
+		s1.calcularCuota();
+		System.out.println(s1.getCuota());
 		
 		
 		
--- src/datos/Socio.java
+++ src/datos/Socio.java
@@ -11,11 +11,14 @@ package datos;
 public class Socio extends Persona{
 
 	private int numeroSocio;
+	private String tipoCuota;
+	private int cuota;
 	
 	
-	public Socio(String dni, String nombre, String apellido, int telefono, String direccion, int numeroSocio) {
+	public Socio(String dni, String nombre, String apellido, int telefono, String direccion, int numeroSocio, String tipoCuota) {
 		super(dni, nombre, apellido, telefono, direccion);
 		this.numeroSocio=numeroSocio;
+		this.tipoCuota=tipoCuota;
 	}
 
 
@@ -27,6 +30,45 @@ public class Socio extends Persona{
 	public void setNumeroSocio(int numeroSocio) {
 		this.numeroSocio = numeroSocio;
 	}
+
+
+	public String getTipoCuota() {
+		return tipoCuota;
+	}
+
+
+	public void setTipoCuota(String tipoCuota) {
+		this.tipoCuota = tipoCuota;
+	}
 	
+	
+	
+	public int getCuota() {
+		return cuota;
+	}
+
+
+	public void setCuota(int cuota) {
+		this.cuota = cuota;
+	}
+
+
+	public void calcularCuota() {
+		int cuotaAnual=960;
+		switch (tipoCuota) {
+		case "Anual": 
+			this.cuota=cuotaAnual;
+			break;
+		case "Semestral":
+			this.cuota=cuotaAnual/2;
+			break;
+		case "Trimestral":
+			this.cuota=cuotaAnual/4;
+			break;
+		case "Mensual":
+			this.cuota=cuotaAnual/12;
+			break;
+		}
+	}
 	
 }
