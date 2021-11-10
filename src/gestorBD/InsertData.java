f90cb13a94f31644fa7329e4e00de9deddd9fed7
malensanz <malensanz@opendeusto.es>
2021-11-10 18:36:41 +0100

    Add insert datas

--- src/gestorBD/InsertData.java
+++ src/gestorBD/InsertData.java
@@ -34,6 +34,35 @@ public class InsertData {
 	
 	public static void main(String[] args) {
 		insertSocio("72451234A", "Ana", "Sanchez", 687333222, "Avenida 8", 20, "Anual", 960);
+		insertSocio("71233123D", "Jon", "Agirre", 677345233, "Gipuzkoa kalea 5A, 1A", 1, "Anual", 960);
+		insertSocio("72555234T", "Mikel", "Etxabe", 688883222, "Avenida Navarra 39B, 2A", 2, "Anual", 960);
+		insertSocio("44533123D", "Ainhoa", "Agirre", 677345290, "Balea Kalea 5, 1A", 3, "Anual", 960);
+		insertSocio("72225111X", "Maria", "Aramburu", 688090210, "Avenida de la Libertad 8B, 2A", 4, "Semestral", 960);
+		insertSocio("71233883W", "Juan Carlos", "Agirre", 601455233, "Avenida San Francisco 12, 4C", 5, "Anual", 960);
+		insertSocio("43551234A", "Jorge", "Legorburu", 677373222, "Avenida Gipuzcoa 32, 5A", 6, "Mensual", 960);
+		insertSocio("44233443P", "Jon", "Etxabe", 670015233, "Avenida Zumalakarregi 5C, 3A", 7, "Anual", 960);
+		insertSocio("77322234Q", "Iñigo", "Agirrezabalaga", 687303200, "Avenida Alava 3, 2A", 8, "Anual", 960);
+		insertSocio("43233553S", "June", "Merketegi", 677775637, "Avenida Zarautz 2, 1B", 9, "Trimestral", 960);
+		insertSocio("72221224V", "Marina", "Etxeberria", 600102562, "Avenida Navarra 4, 3C", 10, "Anual", 960);
+		insertSocio("71221623H", "Carlos", "Manterola", 607341231, "Zuberoa kalea 5, 1C", 11, "Mensual", 960);
+		insertSocio("42453234S", "Carla", "Diaz", 617333212, "Gipuzkoa plaza 8, 2A", 12, "Anual", 960);
+		insertSocio("76663123D", "Karmele", "Zubiaurre", 607445233, "Plaza del Ayuntamiento 5, 3A", 13, "Anual", 960);
+		insertSocio("43451794J", "Miguel", "Atxa", 680333002, "Avenida Colón 13, 1C", 14, "Anual", 960);
+		insertSocio("44234123L", "David", "Erro", 600145231, "Lauaxeta kalea 5B, 3B", 4, "Anual", 960);
+		insertSocio("44851234T", "Gaizka", "Perez", 697333092, "Calle Axular 8, 1B", 15, "Anual", 960);
+		insertSocio("43233893P", "Cristina", "Jaio", 600345013, "Avenida Navarra 22, 4C", 16, "Semestral", 960);
+		insertSocio("44651234B", "Alejandro", "Landa", 687333222, "Avenida Barcelona 1, 8A", 17, "Anual", 960);
+		insertSocio("43232623K", "Ines", "Bernal", 677646233, "Calle Mitxelena 5A, 2A", 18, "Anual", 960);
+		insertSocio("71411234H", "Elisa", "Artola", 607833822, "Avenida Gipuzcoa 22, 4A", 19, "Mensual", 960);
+		insertSocio("42233123G", "Miren", "Arregi", 699345233, "Avenida Alava 5, 4A", 21, "Trimestral", 960);
+		insertSocio("42451434C", "Ignacio", "Manterola", 617333219, "Calle Lauaxeta 5A, 3A", 22, "Anual", 960);
+		insertSocio("78283128N", "Malen", "Bengoetxea", 677643233, "Kale Nagusia 1, 5B", 23, "Anual", 960);
+		insertSocio("43451234U", "Miguel Angel", "Azcue", 601303222, "Calle Mendilauta 2, 2A", 24, "Anual", 960);
+		insertSocio("44234123F", "Aitziber", "Zabala", 677945293, "Indamendi Kalea 5, 6C", 25, "Semestral", 960);
+		insertSocio("71858234M", "Peru", "Diez", 688883282, "Indamendi Kalea 8, 1B", 26, "Anual", 960);
+		insertSocio("44234423X", "Asier", "Etxabe", 677695263, "Avenida Navarra 11, 5A", 27, "Trimestral", 960);
+		insertSocio("72441284I", "Ignacio", "Illarramendi", 601103222, "Calle Iturribide 8, 3A", 28, "Anual", 960);
+		insertSocio("71166123X", "Juan", "Rodriguez", 607340730, "Calle Lampardo 5, 4B", 29, "Mensual", 960);
 	}
 	
 }
