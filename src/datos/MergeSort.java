2344788b0982a568daa584768fa11059a4597589
malensanz <malensanz@opendeusto.es>
2021-12-01 12:01:14 +0100

    mergesort

---
+++ src/datos/MergeSort.java
@@ -0,0 +1,13 @@
+package datos;
+
+public class MergeSort implements Busqueda {
+	
+	//trivial: array.lenght==0 or ==1
+	//no trivial: partir, m izq, m der, mezclar
+
+	@Override
+	public int[] buscar(int[] array, int low, int high) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
