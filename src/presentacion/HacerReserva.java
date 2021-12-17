c8efbbc819beea8d3f976e00853b1f26599fca6d
malensanz <malensanz@opendeusto.es>
2021-12-17 16:18:45 +0100

    parte socio corregida

--- src/presentacion/HacerReserva.java
+++ src/presentacion/HacerReserva.java
@@ -25,10 +25,7 @@ import java.awt.event.ActionEvent;
 
 public class HacerReserva extends JFrame {
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
+/*	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -39,12 +36,12 @@ public class HacerReserva extends JFrame {
 				}
 			}
 		});
-	}
+	}*/
 
 	/**
 	 * Create the frame.
 	 */
-	public HacerReserva() {
+	public HacerReserva(JFrame ventanaAnterior, Socio socio) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 630, 477);
 		getContentPane().setLayout(null);
